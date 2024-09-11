f04f1f455696c27f434f43bb3f13d2b0041705aa
Amarchand K <amarchandk@theecode.xyz>
2024-09-11 18:56:39 +0530

    country length changed

--- lib/countries.dart
+++ lib/countries.dart
@@ -5952,7 +5952,7 @@ const List<Country> countries = [
     code: "SA",
     dialCode: "966",
     minLength: 9,
-    maxLength: 9,
+    maxLength: 11,
   ),
   Country(
     name: "Senegal",
@@ -7130,7 +7130,7 @@ const List<Country> countries = [
     code: "AE",
     dialCode: "971",
     minLength: 9,
-    maxLength: 9,
+    maxLength: 10,
   ),
   Country(
     name: "United Kingdom",
